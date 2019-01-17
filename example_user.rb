1810203db93d564911bf5ad41f3adfc3e9c16704
kawanaka <kawanaka_s@tenso.com>
2019-01-17 16:52:28 +0900

    alphabetical_nameメソッドを定義

--- example_user.rb
+++ example_user.rb
@@ -11,6 +11,10 @@ class User
     @full_name = "#{@first_name} #{@last_name}"
    end
 
+  def alphabetical_name
+    @alphabetical_name = "#{@last_name}, #{@first_name}"
+  end
+
   def formatted_email
     "#{@full_name} <#{@email}>"
   end
