c3a3659d25e4c64e285b3bd642b54ec2d297ab42
kawanaka <kawanaka_s@tenso.com>
2019-01-21 14:30:07 +0900

    name属性、email属性の長さ検証

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,9 +9,19 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
-  
+
   test "name should be present" do
     @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
 end
